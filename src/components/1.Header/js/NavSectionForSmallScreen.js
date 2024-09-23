2682a04c987c3b77b0c97d562e702b378f95fe9c
Benihin_Dev <148499823+Benihin-Dev@users.noreply.github.com>
2024-09-23 21:26:12 +0530

    Update NavSectionForSmallScreen.js

--- src/components/1.Header/js/NavSectionForSmallScreen.js
+++ src/components/1.Header/js/NavSectionForSmallScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavSectionForSmallScreen() {
   return (
-    <div className=" w-full fixed top-0 h-screen bg-black left-0">
+    <div className=" w-full fixed top-0 h-screen bg-transparent left-0">
       NavSectionForSmallScreen
     </div>
   );
